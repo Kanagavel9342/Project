549926ba3d22d44fbd6cd300e863beda9b7ef7e7
kanagavel <skvel.fx@gmail.com>
2025-05-15 00:50:53 +0530

    ///

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FiShoppingCart, FiPackage, FiLayers, FiUser, FiLogOut } from 'react-icons/fi';
-import './Dashboard.css';
+import './dashboard.css';
 
 const Dashboard = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
